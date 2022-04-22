7a81d770dd75be457201fa3b6f2c0a494c428623
mr-beale <beal17044@byui.edu>
2022-04-22 13:15:04 -0600

    Moved Js to stand alone file. Updated CSS with media queries.

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+let year = new Date().getFullYear();
+document.getElementById("year").innerHTML = year;
+
+let savedate = document.lastModified;
+document.getElementById("updated").innerHTML = "Last Updated: "+ savedate;
