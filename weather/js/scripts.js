3285cc951f866fe59fa149974ad5e8dc2e95414e
mr-beale <beal17044@byui.edu>
2022-04-30 16:46:23 -0600

    Updated Date format in javascript

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -1,8 +1,17 @@
-let year = new Date().getFullYear();
-document.getElementById("year").innerHTML = year;
+let thisYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = thisYear;
 
-let savedate = document.lastModified;
-document.getElementById("updated").innerHTML = "Last Updated: "+ savedate;
+
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let savedate = new Date(document.lastModified);
+let month = savedate.toLocaleString("en-US", {month: "long"});
+let date = savedate.getDate();
+let year = savedate.getFullYear();
+let day = savedate.getDay();
+let weekday = weekdays[savedate.getDay()];
+let lastChanged = weekday+", "+date+" "+month+" "+year; 
+
+document.getElementById("updated").innerHTML = "Last Updated: "+ lastChanged;
 
 function toggleMenu() {
     document.getElementsByClassName("menu")[0].classList.toggle("responsive");
